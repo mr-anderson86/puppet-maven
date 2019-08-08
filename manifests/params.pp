b28cb33f5e581580bad7354375b470fbaa63eb2e
Dovi Klausner <39731910+mr-anderson86@users.noreply.github.com>
2019-08-08 20:22:33 +0300

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,8 @@ class maven::params {
   $package_name     = "apache-maven"
   $package_version  = "3.6.0"
   $archive_format   = "tar.gz"
-  $download_url     = "https://www-us.apache.org/dist/maven/maven-3/${package_version}/binaries/${package_name}-${package_version}-bin.${archive_format}"
+  $package_file     = "${package_name}-${package_version}-bin.${archive_format}"
+  $download_url     = "https://www-us.apache.org/dist/maven/maven-3/${package_version}/binaries/${package_file}"
   $install_path     = "/opt"
   $java_binary      = "java-1.8.0-openjdk-devel"
   
