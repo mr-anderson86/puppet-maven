ac472b4a89025bb71ed4ddfb936656c64d0d696b
Dovi Klausner <39731910+mr-anderson86@users.noreply.github.com>
2019-08-08 20:19:31 +0300

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,12 @@
 class maven (
 
+  $package_name     = $maven::params::package_name,
+  $package_version  = $maven::params::package_version,
+  $archive_format   = $maven::params::archive_format,
+  $download_url     = $maven::params::download_url,
+  $install_path     = $maven::params::install_path,
+  $java_binary      = $maven::params::install_path::java_binary,
+
 ) inherits maven::params {
 
 }
