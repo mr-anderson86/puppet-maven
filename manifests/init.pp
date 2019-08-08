1c4cbf4638ab571cb2a337df5ef79aefc59b4f43
Dovi Klausner <39731910+mr-anderson86@users.noreply.github.com>
2019-08-08 20:22:53 +0300

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,7 @@ class maven (
   $package_name     = $maven::params::package_name,
   $package_version  = $maven::params::package_version,
   $archive_format   = $maven::params::archive_format,
+  $package_file     = $maven::params::package_file,
   $download_url     = $maven::params::download_url,
   $install_path     = $maven::params::install_path,
   $java_binary      = $maven::params::install_path::java_binary,
